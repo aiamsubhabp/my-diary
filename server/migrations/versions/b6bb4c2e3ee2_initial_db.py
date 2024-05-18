653a9721fb38110c9aac652a17a4b82e1ebf8976
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2024-05-18 16:07:21 -0400

    initial commit

---
+++ server/migrations/versions/b6bb4c2e3ee2_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: b6bb4c2e3ee2
+Revises: 
+Create Date: 2024-05-18 16:07:15.674325
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b6bb4c2e3ee2'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
