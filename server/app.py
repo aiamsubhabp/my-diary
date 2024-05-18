0a9a5af5df071cb49c91e40c5e1e31642328442e
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2024-05-18 18:50:25 -0400

    create routes for signup, login, logout, checksession

--- server/app.py
+++ server/app.py
@@ -1,6 +1,63 @@
 from config import app
+from flask import request, session
+from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
 
+from config import app, db, api
 from models import User, Entry
 
+class Signup(Resource):
+	def post(self):
+		request_json = request.get_json()
+
+		username = request_json.get('username')
+		password = request_json.get('password')
+
+		user = User(
+		username = username
+		)
+
+		user.password_hash = password
+		
+		try:
+			db.session.add(user)
+			db.session.commit()
+
+			session['user_id'] = user.id
+			return user.to_dict(), 201
+		except IntegrityError:
+			return {'error': '422 Unprocessable Entity'}, 422
+    
+class CheckSession(Resource):
+	def get(self):
+		if session.get('user_id'):
+			user = User.query.filter(User.id == session['user_id']).first()
+			return user.to_dict(), 200
+		return {'error': '401 Unauthorized'}, 401
+
+class Login(Resource):
+	def post(self):
+		request_json = request.get_json()
+
+		username = request_json.get('username')
+		password = request_json.get('password')
+
+		user = User.query.filter(User.username == username).first()
+		if user:
+			if user.authenticate(password):
+				session['user_id'] = user.id
+				return user.to_dict(), 200
+		return {'error': '401 Unauthorized'}, 401
+
+class Logout(Resource):
+	def delete(self):
+		session['user_id'] = None
+		return {}, 204
+    
+api.add_resource(Signup, '/api/signup')
+api.add_resource(CheckSession, '/api/check_session')
+api.add_resource(Login, '/api/login')
+api.add_resource(Logout, '/api/logout')
+
 if __name__ == "__main__":
-  app.run(port=5555, debug=True)
+	app.run(port=5555, debug=True)
